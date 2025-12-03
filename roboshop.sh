1ab50c16562919dde2f1900f300099afc8e3e22e
k chakradhar <kchakradhar710@gmail.com>
2025-12-03 12:12:49 +0530

    colors

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-00c30ba058098ea3b"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "fronted")
+ZONE_ID="Z023666325XPYAQAW6FZN"
+DOMAIN_NAME="chakri.icu"
+
+for instance in ${INSTANCES[@]}
+do
+   INSTADNCES_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-00c30ba058098ea3b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].Instance Id" --output text)
+    if [ $instance != "frontend" ]
+    then
+       IP=aws ec2 describe-instances --instance-ids $INSTADNCES_ID --query 'Reservations[0]Instances[0].PrivateIpAddress' --output text
+    else
+        IP=aws ec2 describe-instances --instance-ids $INSTADNCES_ID --query 'Reservations[0]Instances[0].PrivateIpAddress' --output text
+    fi    
+    echo "$instance IP address: $IP"
+done
