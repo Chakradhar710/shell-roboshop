44becd496d9ee0ae803ecccd60e1d7624603e584
k chakradhar <kchakradhar710@gmail.com>
2025-12-03 18:02:21 +0530

    hello world started

--- frontend.sh
+++ frontend.sh
@@ -33,34 +33,34 @@ VALIDATE(){
 }
 
 
-dnf module disable nginx -y  &>>$LOGS_FLODER
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
 
-dnf module enable nginx:1.24 -y &>>$LOGS_FLODER
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx:1.24"
 
-dnf install nginx -y &>>$LOGS_FLODER
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOGS_FLODER
+systemctl enable nginx  &>>$LOG_FILE
 systemctl start nginx 
-VALIDATE $? "Starting Nginx" 
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOGS_FLODER
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FLODER
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOGS_FLODER
-VALIDATE $? "unzipping forntend"
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzipping frontend"
 
-rm -rf /etc/nginx/nginx.conf &>>$LOGS_FLODER
-VALIDATE $? "Remove default nginx cong"
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
-systemctl restart nginx
+systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
