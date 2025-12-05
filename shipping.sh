f326ac200e32917d013dba3a6f570daeae230f81
k chakradhar <kchakradhar710@gmail.com>
2025-12-05 19:02:31 +0530

    hello world started

--- shipping.sh
+++ shipping.sh
@@ -82,9 +82,9 @@ VALIDATE $? "Starting shipping"
 dnf install mysql -y &>>LOG_FILE
 VALIDATE $? "Installing MYSQL"
 
-mysql -h mysql.chakri.icu -uroot -pRoboshop@1 < /app/db/schema.sql &>>LOG_FILE
-mysql -h mysql.chakri.icu -uroot -pRoboshop@1 < /app/db/app-user.sql &>>LOG_FILE
-mysql -h mysql.chakri.icu -uroot -pRoboshop@1 < /app/db/master-data.sql &>>LOG_FILE
+mysql -h mysql.chakri.icu -uroot -$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>LOG_FILE
+mysql -h mysql.chakri.icu -uroot -$YSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>LOG_FILE
+mysql -h mysql.chakri.icu -uroot -$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>LOG_FILE
 VALIDATE $? "Loading data into MYSQL"
 
 systemctl restart shipping &>>LOG_FILE
