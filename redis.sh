7970250a2f9510743c49915d99e8128e3ffe5845
k chakradhar <kchakradhar710@gmail.com>
2025-12-05 15:35:32 +0530

    hello world started

--- redis.sh
+++ redis.sh
@@ -33,11 +33,11 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Redis version"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Redis:7"
 
-dnf install redis -y 
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Instaling Redis"
