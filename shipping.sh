6711db9178ece057389b6dfcd6066f04b1e21f79
k chakradhar <kchakradhar710@gmail.com>
2025-12-07 11:53:51 +0530

    hello world started

--- shipping.sh
+++ shipping.sh
@@ -80,7 +80,7 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.chakri.icu -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     mysql -h mysql.chakri.icu -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
